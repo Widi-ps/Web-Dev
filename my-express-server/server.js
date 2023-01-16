e08b0c803b07ac2300976d719bb1f837e90dd129
widi-ps <widyapujisaputro@gmail.com>
2023-01-16 14:35:48 +0700

    learn route

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -6,6 +6,18 @@ app.get("/", function(req, res){
   res.send("<h1>Hello, World</h1>")
 })
 
+app.get("/contact", function(req, res){
+  res.send("Contact me at: haha@gmail.co.id")
+})
+
+app.get("/about", function(req, res){
+  res.send("My name is Widi and I love to swim and see movies")
+})
+
+app.get("/hobbies", function(req, res){
+  res.send("<ul><li>Swimming</li><li>Movies</li></ul>")
+})
+
 app.listen(3000, function()
   {
     console.log("Server started on port 3000");
