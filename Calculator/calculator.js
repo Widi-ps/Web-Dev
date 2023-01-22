1c62109be52c8f598e18dfc8e0db94224f93cc2a
widi-ps <widyapujisaputro@gmail.com>
2023-01-22 13:04:04 +0700

    add route bmi calculator

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -16,6 +16,19 @@ app.post("/", function (req, res) {
   res.send("The result of the calculation is " + result);
 });
 
+app.get("/bmicalculator", function (req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function (req, res) {
+  const weight = parseFloat(req.body.weight);
+  const height = parseFloat(req.body.height);
+
+  const result = weight / Math.pow(height, 2);
+
+  res.send("Your BMI is " + result);
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
