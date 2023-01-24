d6190a684b3721fbb5aba5567a6eb5a9a57c9472
widi-ps <widyapujisaputro@gmail.com>
2023-01-24 19:15:31 +0700

    setup package

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
 
-const app = express()
-
-const https = require('https');
+const app = express();
 
 app.listen(3000, function () {
-  console.log("Server is running on port 3000")
-})
+  console.log("Server is running on port 3000");
+});
