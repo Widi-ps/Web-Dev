f57d20cc29059197d8189a22a988f827f17c2eea
widi-ps <widyapujisaputro@gmail.com>
2023-03-09 23:17:14 +0700

    add status code

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -8,12 +8,15 @@ const requestListener = (req, res) => {
 
   if (url === "/") {
     if (method === "GET") {
+      res.statusCode = 200;
       res.end("<h1>Ini adalah homepage</h1>");
     } else {
+      res.statusCode = 400;
       res.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
     }
   } else if (url === "/about") {
     if (method === "GET") {
+      res.statusCode = 200;
       res.end("<h1>Halo! Ini adalaha halaman about</h1>");
     } else if (method === "POST") {
       let body = [];
@@ -25,14 +28,17 @@ const requestListener = (req, res) => {
       req.on("end", () => {
         body = Buffer.concat(body).toString();
         const { name } = JSON.parse(body);
+        res.statusCode = 200;
         res.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
       });
     } else {
+      res.statusCode = 400;
       res.end(
         `<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`
       );
     }
   } else {
+    res.statusCode = 404;
     res.end("<h1>Halaman tidak ditemukan!</h1>");
   }
 };
