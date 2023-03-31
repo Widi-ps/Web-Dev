6c4b8230dc14cbc83c3f0af29993976b32282083
widi-ps <widyapujisaputro@gmail.com>
2023-03-31 21:41:23 +0700

    feat: view the note

--- notes-app-backend/src/handler.js
+++ notes-app-backend/src/handler.js
@@ -1,39 +1,75 @@
 const { nanoid } = require("nanoid");
 const notes = require("./notes");
 
-const addNoteHandler = (req, h) => {
-  const { title, tags, body } = req.payload;
+const addNoteHandler = (request, h) => {
+  const { title, tags, body } = request.payload;
 
   const id = nanoid(16);
   const createdAt = new Date().toISOString();
   const updateAt = createdAt;
 
   const newNote = {
-    title, tags, body, id, createdAt, updateAt
+    title,
+    tags,
+    body,
+    id,
+    createdAt,
+    updateAt,
   };
 
-  notes.push(newNote)
+  notes.push(newNote);
 
-  const isSuccess = notes.filter((note) => note.id === id).length > 0
+  const isSuccess = notes.filter((note) => note.id === id).length > 0;
 
   if (isSuccess) {
     const response = h.response({
-      status: 'success',
-      message: 'Catatan berhasil ditambahkan',
+      error: false,
+      status: "success",
+      message: "Catatan berhasil ditambahkan",
       data: {
         noteId: id,
-      }
-    })
+      },
+    });
     response.code(201);
-    return response
+    response.header("Access-Control-Allow-Origin", "*");
+    return response;
   }
 
   const response = h.response({
-    status: 'fail',
-    message: 'Catatan gagal ditambahkan',
-  })
-  response.code(500)
-  return response
+    status: "fail",
+    message: "Catatan gagal ditambahkan",
+  });
+  response.code(500);
+  return response;
 };
 
-module.exports = { addNoteHandler };
+const getAllNotesHandler = () => ({
+  status: "success",
+  data: {
+    notes,
+  },
+});
+
+const getNoteByIdHandler = (request, h) => {
+  const { id } = request.params;
+
+  const note = notes.filter((n) => n.id === id)[0];
+
+  if (note !== undefined) {
+    return {
+      status: "success",
+      data: {
+        note,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: "fail",
+    message: "Catatan tidak ditemukan",
+  });
+  response.code(404);
+  return response;
+};
+
+module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler};
--- notes-app-backend/src/routes.js
+++ notes-app-backend/src/routes.js
@@ -1,10 +1,25 @@
-const { addNoteHandler } = require("./handler");
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler } = require("./handler");
 
 const routes = [
   {
-    method: 'POST',
-    path: '/notes',
+    method: "POST",
+    path: "/notes",
     handler: addNoteHandler,
+    options: {
+      cors: {
+        origin: ["*"],
+      },
+    },
+  },
+  {
+    method: "GET",
+    path: "/notes",
+    handler: getAllNotesHandler,
+  },
+  {
+    method: "GET",
+    path: "/notes/{id}",
+    handler: getNoteByIdHandler,
   },
 ];
 
