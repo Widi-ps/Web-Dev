41b3349d17cfbefc5516b98e9078d96add4638a7
widi-ps <widyapujisaputro@gmail.com>
2023-08-06 12:23:45 +0700

    learn ejs

---
+++ 4.0+EJS/4.0_EJS/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  const today = new Date();
+  const day = 2
+
+  let type = ""
+  let adv = ""
+
+  if (day >= 1 && day <= 5) {
+    type = "a weekday";
+    adv = "it's time to work hard"
+  } else {
+    type = "the weekend";
+    adv = "it's time to play some valorant"
+  }
+
+  console.log(day);
+
+  res.render("index.ejs", {
+    dayType : type,
+    advice: adv,
+  })
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
