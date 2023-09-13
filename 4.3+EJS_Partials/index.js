97e2ed30dffb5bfbe410d812f3cc7bd080761fac
widi-ps <widyapujisaputro@gmail.com>
2023-09-13 11:05:39 +0700

    add routes and the partial to the contact.ejs and about.ejs

--- 4.3+EJS_Partials/index.js
+++ 4.3+EJS_Partials/index.js
@@ -16,6 +16,14 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
