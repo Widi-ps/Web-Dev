93e1d3e91ca9361a918883b94f6d83aa250f89c9
widi-ps <widyapujisaputro@gmail.com>
2023-09-14 09:50:53 +0700

    add logic for the generate band name

--- 4.4+Band_Generator_Project/index.js
+++ 4.4+Band_Generator_Project/index.js
@@ -8,7 +8,7 @@ const port = 3000;
 //Hint 1: CSS files are static files!
 //Hint 2: The header and footer are partials.
 //Hint 3: Add the CSS link in header.ejs
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 //Step 4 - Add a dynamic year to the footer.
 //Hint: Google to find out how to get the current year using JS.
@@ -28,6 +28,12 @@ app.post("/submit", (req, res) => {
   //scroll down to see the two arrays.
   //2. Send the index.ejs as a response and add the adjective and noun to the res.render
   //3. Test to make sure that the random words display in the h1 element in index.ejs
+  const randomAdj = adj[Math.floor(Math.random() * adj.length)];
+  const randomNoun = adj[Math.floor(Math.random() * noun.length)];
+  res.render("index.ejs", {
+    adjective : randomAdj,
+    noun : randomNoun,
+  })
 });
 
 app.listen(port, () => {
