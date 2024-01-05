8160e858f237548a8e962e7baefe37642a70ff34
widi-ps <widyapujisaputro@gmail.com>
2024-01-05 20:57:47 +0700

    add get random joke

--- 6.0_DIY_API/index.js
+++ 6.0_DIY_API/index.js
@@ -8,12 +8,17 @@ const masterKey = "4VGP2DN-6EWM4SJ-N6FGRHV-Z3PR3TT";
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //1. GET a random joke
+app.get("/random", (req, res) => {
+  const random = Math.floor(Math.random() * jokes.length);
+  res.json(jokes[random]);
+});
 
-//2. GET a specific joke
+//2. GET" a specific joke
 
 //3. GET a jokes by filtering on the joke type
 
 //4. POST a new joke
+
 
 //5. PUT a joke
 
