80729ac9fccd026059b0ec5a1012464b0275de62
widi-ps <widyapujisaputro@gmail.com>
2024-01-05 22:11:03 +0700

    delete spesific joke

--- 6.0_DIY_API/index.js
+++ 6.0_DIY_API/index.js
@@ -69,6 +69,20 @@ app.patch("/jokes/:id", (req, res) => {
 });
 
 //7. DELETE Specific joke
+app.delete("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = jokes.findIndex((joke) => joke.id === id);
+
+  if (index !== -1) {
+    jokes.splice(index, 1);
+    console.log(jokes[index]);
+    res.sendStatus(200);
+  } else {
+    res
+      .status(404)
+      .json({ error: `Joke with id ${id} not found. No jokes were deleted` });
+  }
+});
 
 //8. DELETE All jokes
 
