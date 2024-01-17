99f10d24dc907d48bed690e56455cc547559eef7
widi-ps <widyapujisaputro@gmail.com>
2024-01-17 21:38:35 +0700

    add checkVisited function

--- 8.3_Travel_Tracker/index.js
+++ 8.3_Travel_Tracker/index.js
@@ -15,21 +15,27 @@ const db = new pg.Client({
 
 db.connect();
 
-// let countries = []
+async function checkVisisted() {
+  const result = await db.query("SELECT country_code FROM visited_country");
+  let countries = [];
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  return countries
+}
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
   //Write your code here.
-  const result = await db.query("SELECT country_code FROM visited_country");
-  let countries = [];
-  result.rows.forEach((country) => {
-    countries.push(country.country_code);
-  });
-  console.log(result.rows);
+  const countries = await checkVisisted() 
   res.render("index.ejs", { countries: countries, total: countries.length});
 });
+
+app.post("/add", async (req, res) => {
+  
+})
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
