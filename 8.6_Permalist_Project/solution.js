bb95717e5655ec51d99a02d04716fb6fe5743d01
widi-ps <widyapujisaputro@gmail.com>
2024-01-23 12:33:53 +0700

    setup postgres

--- 8.6_Permalist_Project/solution.js
+++ 8.6_Permalist_Project/solution.js
@@ -8,8 +8,8 @@ const port = 3000;
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
-  database: "permalist",
-  password: "123456",
+  database: "Permalist",
+  password: "widi",
   port: 5432,
 });
 db.connect();
